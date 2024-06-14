27c0d78c332a385fe54924767c704f890f24dfb7
Guryss <lala1470@konkuk.ac.kr>
2024-06-15 04:28:25 +0900

    [Feat] #3 - wheel Picker 로직 구현

--- PoCaJeong/PoCaJeong/View/InputContentsView.swift
+++ PoCaJeong/PoCaJeong/View/InputContentsView.swift
@@ -7,19 +7,26 @@
 
 import SwiftUI
 
-enum MusicStyle: String, CaseIterable, Identifiable {
-    case hiphop, kPop, ballad, piano
-    var id: Self { self }
-}
-
 struct InputContentsView: View {
+    // 카페 이름
     @State private var cafeName: String = ""
-    @State private var wifiSpeed: String = ""
-    @State private var concent: String = "00"
-    @State private var nunchi: String = "00"
+    
+    // 와이파이 속도
+    @State private var wifiSpeed: Double = 11.16
+    
+    // 1명당 콘센트 개수
+    @State private var concent: Int = 0
+    @State private var showConcentPicker: Bool = false
+    
+    // 사장님과 눈 마주친 횟수
+    @State private var nunchi: Int = 0
+    @State private var showNunchiPicker: Bool = false
+    
+    // 노래 스타일
+    @State private var musicStyle: SongStyleType?
     
     let sample: [String] = ["00", "01", "02"]
-    @State private var musicStyle: MusicStyle = .hiphop
+    
     
     var body: some View {
         NavigationStack {
@@ -35,48 +42,87 @@ struct InputContentsView: View {
                             
                             Spacer()
                             
-                            Text("000 Mbps")
+                            Text("\(wifiSpeed) Mbps")
                                 .foregroundStyle(.secondary)
                         }
                     }
+                    
+                    let isTenConcent: Bool = concent == 10
+                    let isTenNunchi: Bool = nunchi == 10
                     
                     Section {
                         HStack {
                             Text("콘센트")
                             Spacer()
-                            Text("1명당 콘센트 \(concent)개")
+                            Button(action: {
+                                showConcentPicker.toggle()
+                            }, label: {
+                                Text(isTenConcent ? "1명당 콘센트 \(concent)개" : "1명당 콘센트 0\(concent)개")
+                                    .foregroundStyle(showConcentPicker ? .pink : .black)
+                                
+                            })
+                            .buttonStyle(.bordered)
                         }
                         
-                        Picker("1명당 콘센트 00개", selection: $concent) {
-                            ForEach(sample, id: \.self) {
-                                Text($0)
+                        if showConcentPicker {
+                            Picker("", selection: $concent) {
+                                ForEach(0..<11) { index in
+                                    if index == 10 {
+                                        Text("\(index)")
+                                    }
+                                    else {
+                                        Text("0\(index)")
+                                    }
+                                }
                             }
+                            .pickerStyle(.wheel)
                         }
-                        .pickerStyle(.wheel)
                     }
                     
                     Section {
                         HStack {
                             Text("눈치")
                             Spacer()
-                            Text("사장님과 눈 마주친 횟수 \(nunchi)번")
+                            Button(action: {
+                                showNunchiPicker.toggle()
+                            }, label: {
+                                Text(isTenNunchi ? "사장님과 눈 마주친 횟수 \(nunchi)번" : "사장님과 눈 마주친 횟수 0\(nunchi)번")
+                                    .foregroundStyle(showNunchiPicker ? .pink : .black)
+                            })
+                            .buttonStyle(.bordered)
                         }
                         
-                        Picker("1명당 콘센트 00개", selection: $nunchi) {
-                            ForEach(sample, id: \.self) {
-                                Text($0)
+                        if showNunchiPicker {
+                            Picker("", selection: $nunchi) {
+                                ForEach(0..<11) { index in
+                                    if index == 10 {
+                                        Text("\(index)")
+                                    }
+                                    else {
+                                        Text("0\(index)")
+                                    }
+                                }
                             }
+                            .pickerStyle(.wheel)
                         }
-                        .pickerStyle(.wheel)
                     }
                     
                     Section {
                         HStack {
                             Text("노래 스타일")
+                            
                             Spacer()
+                            
                             Picker("", selection: $musicStyle) {
-                                ForEach(MusicStyle.allCases) { music in
-                                    Text(music.rawValue)
+                                if musicStyle == nil {
+                                    Text("없음")
+                                }
+                                Divider()
+                                
+                                Section {
+                                    ForEach(SongStyleType.allCases) { music in
+                                        Text(music.rawValue)
+                                    }
                                 }
                             }
                         }
