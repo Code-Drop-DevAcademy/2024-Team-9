5a5d358394c26c737de729d3444c1a7e900cba9a
Xerath <yoondj98@naver.com>
2024-06-14 23:59:30 +0900

    init project

---
+++ PoCaJeong/PoCaJeong/PoCaJeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoCaJeongApp.swift
+//  PoCaJeong
+//
+//  Created by 윤동주 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PoCaJeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
