2b65ea35e08d781748fb447fd7359a73492a064a
Xerath <yoondj98@naver.com>
2024-06-15 03:12:08 +0900

    feat: CafeListView 구현

---
+++ PoCaJeong/PoCaJeong/View/CafeListView.swift
@@ -0,0 +1,44 @@
+//
+//  CafeListView.swift
+//  PoCaJeong
+//
+//  Created by 윤동주 on 6/15/24.
+//
+
+import SwiftUI
+
+struct CafeListView: View {
+    @State var cafes = MockCafeData.cafes
+    @State var isInputContentsViewShown = false
+    var body: some View {
+        ZStack {
+            Color(hex: "F2F2F7")
+                .ignoresSafeArea()
+            VStack {
+                List(cafes) { cafe in
+                    NavigationLink {
+                        CafeDetailView(cafe: cafe)
+                    } label: {
+                        Text(cafe.name)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.top, 18)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    isInputContentsViewShown = true
+                }, label: {
+                    
+                })
+                Image(systemName: "plus")
+                    .foregroundStyle(.blue)
+            }
+        }
+        .navigationTitle("CafeListView")
+        .toolbarBackground(.white, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+    }
+}
