3d26eef67474503897368bbb006d140b2e21ad03
Guryss <lala1470@konkuk.ac.kr>
2024-06-15 09:37:09 +0900

    [Add] #7 - 필요한 이미지 에셋 및 Font, Font Extension 추가

---
+++ PoCaJeong/PoCaJeong/Helper/Extensions/Font+.swift
@@ -0,0 +1,8 @@
+//
+//  Font+.swift
+//  PoCaJeong
+//
+//  Created by Seoyeon Choi on 6/15/24.
+//
+
+import Foundation
---
+++ PoCaJeong/PoCaJeong/View/Preview/PreviewSecondView.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewSecondView.swift
+//  PoCaJeong
+//
+//  Created by Seoyeon Choi on 6/15/24.
+//
+
+import SwiftUI
+
+struct PreviewSecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewSecondView()
+}
